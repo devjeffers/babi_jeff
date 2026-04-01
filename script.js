9b14edb9da82b8cee0ffa955695f7c575435d14d
Jefferson Jimenez <devsfull.jefferson@gmail.com>
2026-04-01 00:21:42 -0400

    automatização de estoque, melhoria para experiência do usuários

--- script.js
+++ script.js
@@ -1,39 +1,38 @@
 let carrinho = [];
 
-//const produtos = [
-    //{ nome: "Bolo Caseiro de Chocolate Cacau 50%" , peso: 1.32, estoque: 10, preco: 65.00 },
-   // { nome: "Bolo Caseiro de Cacau 50% com Maracujá" , peso: 1.32, estoque: 8, preco: 65.00 },
-   // { nome: "Bolo Caseiro de Cacau 50% com Cupu" , peso: 1.32, estoque: 5, preco: 65.00 },
-   // { nome: "Bolo Caseiro Pequeno de Cacau 50% com Cupu" , peso: 0.790, estoque: 5, preco: 45.00 },
-   // { nome: "Bolo Caseiro de Limão" , peso: 0.730, estoque: 5, preco: 45.00 },
-   // { nome: "Bolo Caseiro de Maracujá" , peso: 0.730, estoque: 5, preco: 45.00 }
-//];
+let total = 0;
 
     function carregarprodutos() {
         const div = 
         document.getElementById("produtos");
         
-        //produtos.forEach((produto, index) => {
             div.innerHTML = "";
-            // <div class="produto"> comentei para não aparecer os produtos na tela, mas o código continua aqui caso queira reativar
-            //     <h3>${produto.nome}</h3>
-            //     <p>Peso: ${produto.peso} kg</p>
-            //     <p>Estoque: ${produto.estoque}</p>
-            //     <p>Preço: R$ ${produto.preco.toFixed(2)}</p>
 
-            //     <button onclick="adicionar('${index}')">Adicionar</button>
-            // </div>
-            // `;
-        db.collection("produtos").get().then((snapshot) => {
+        db.collection("produtos").onSnapshot((snapshot) => {
+            div.innerHTML = "";
+
             snapshot.forEach((doc) => {
                 const produto = doc.data();
                 const id = doc.id;
+                
                 div.innerHTML += `
                 <div class="produto">
                 <h3>${produto.nome}</h3>
-                <p>Disponível: ${produto.estoque}</p>
+                <p>
+                ${produto.estoque > 0 
+                    ? `Disponível: ${produto.estoque}` 
+                    : `<span style="color:red;">ESGOTADO</span>`
+                }
+                </p>
                 <p>Preço: R$ ${produto.preco.toFixed(2)}</p>
-                <button onclick="adicionar('${id}', '${produto.nome}', ${produto.preco})">Adicionar</button>
+                ${produto.estoque > 0 
+                ? `<button onclick="adicionar('${id}', '${produto.nome}', ${produto.preco})">
+                        Adicionar
+                    </button>`
+                : `<button disabled style="background: gray;">
+                        Esgotado
+                    </button>`
+                }
                 </div>
                 `;
             });
@@ -41,11 +40,49 @@ let carrinho = [];
     }
 
     function adicionar(id, nome, preco) {
-    carrinho.push({ id, nome, preco });
-    alert("Adicionado ao carrinho!");
+        carrinho.push({ id, nome, preco });
+        
+        total += preco;
+
+        atualizarTotal();
+        atualizarCarrinho(); //novo
+        
+        alert("Adicionado ao carrinho!");
 }
+    function removerItem(index) {
+        total -= carrinho[index].preco;
+        
+        carrinho.splice(index, 1);
+
+        atualizarTotal();
+        atualizarCarrinho();
+    }
+    function atualizarCarrinho() {
+        const div = document.getElementById("carrinho");
+        
+        div.innerHTML = "";
+
+        carrinho.forEach((item, index) => {
+            div.innerHTML += `
+                <p>
+                ${item.nome} - R$ ${item.preco.toFixed(2)}
+                <button onclick="removerItem(${index})">❌</button>
+                </p>
+            `;
+        });
+    }
+
+    function atualizarTotal() {
+        const totalDiv = document.getElementById("total");
+        
+        totalDiv.innerHTML = "Total: R$ " + total.toFixed(2);
+    }
 
     function finalizarPedido() {
+        if (carrinho.length === 0) {
+        alert("Adicione itens ao carrinho!");
+        return;
+}
         const nome = document.getElementById("nome").value;
         const celular = document.getElementById("celular").value;
         const endereco = document.getElementById("endereco").value;
@@ -65,6 +102,24 @@ let carrinho = [];
             return;
         }
 
+        carrinho.forEach((item) => {
+            mensagem += "- " + item.nome + " (R$ " + item.preco.toFixed(2) + ")\n";
+        });
+
+        mensagem += "\n💰 Pagamento: " + pagamento;
+        
+        mensagem += `\n💰 Total: R$ ${total.toFixed(2)}`;
+
+        const numero = "5597981024922";
+
+        const url = 
+        `https://wa.me/${numero}?text=${encodeURIComponent(mensagem)}`;
+
+        if (!nome || !celular || !endereco || !pagamento) {
+            alert("Por favor, preencha todos os campos.");
+            return;
+        }
+
          db.collection("pedidos").add({
             nome: nome,
             celular: celular,
@@ -74,22 +129,45 @@ let carrinho = [];
             criadoEm: new Date()
         }).then(() => {
             console.log("Pedido salvo no Firestore!");
-        })
-        .catch((error) => {
-            console.error("Erro ao salvar pedido: ", error);
+
+            Promise.all(
+            carrinho.map((item) => {
+                const ref = db.collection("produtos").doc(item.id);
+
+                return db.runTransaction(async (transaction) => {
+                    const doc = await transaction.get(ref);
+
+                    if (!doc.exists) {
+                        throw "Produto não existe!";
+                    }
+
+                    let estoqueAtual = doc.data().estoque;
+
+                    if (estoqueAtual <= 0) {
+                        throw `Produto ${item.nome} esgotado!`;
+                    }
+
+                    transaction.update(ref, {
+                        estoque: estoqueAtual - 1
+                    });
+                });
+            })
+            )
+            .then(() => {
+                window.open(url, "_blank");
+
+                carrinho = [];
+                total = 0;
+                atualizarTotal();
+                atualizarCarrinho(); // importante para manter a ordem correta do estoque
+            })
+            .then(() => {
+                console.log("Estoque atualizado com sucesso!");
+            })
+            .catch((error) => {
+                alert(error);
+            });
         });
-
-        carrinho.forEach((item) => {
-            mensagem += "- " + item.nome + " (R$ " + item.preco.toFixed(2) + ")\n";
-        });
-
-        mensagem += "\n💰 Pagamento: " + pagamento;
-
-        const numero = "5597981024922";
-
-        const url = 
-        `https://wa.me/${numero}?text=${encodeURIComponent(mensagem)}`;
-        window.open(url, "_blank");
     }
-     
-        carregarprodutos();
+
+    carregarprodutos();
